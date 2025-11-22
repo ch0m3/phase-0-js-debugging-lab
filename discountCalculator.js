1f415cb033a47b02a86c5053c4999b2a2a69a192
Chome <chomeallan@gmail.com>
2025-11-22 12:14:55 +0300

    debigging

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,14 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
-    let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
-        totalPrice += pricePerItem;
-    }
+    // compute total price as quantity times unit price
+    let totalPrice = quantity * pricePerItem;
 
+    // apply 10% discount when buying 10 or more items
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
 
     return totalPrice;
 }
-
 
 
 module.exports = calculateDiscountedPrice;
